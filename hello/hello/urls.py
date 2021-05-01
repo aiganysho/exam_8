3da496279de64d0ec0f5873ea5c3dd5e9ce81cfd
Aiganysh <aiganyshx@gmail.com>
2021-05-01 15:43:06 +0600

    создано приложение accounts, и указана роутры на главном urls

--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -10,8 +10,8 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    1. Import the include() function: from django.urls.py import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls.py'))
 """
 
 from django.contrib import admin
@@ -25,6 +25,6 @@ HOMEPAGE_URL = 'webapp/'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('webapp/', include('webapp.urls')),
-    # path('accounts/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
     path('', RedirectView.as_view(url=HOMEPAGE_URL, permanent=False)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
