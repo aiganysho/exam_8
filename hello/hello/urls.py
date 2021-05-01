e07a61c0d745266dd172b341eb65d94512d18412
Aiganysh <aiganyshx@gmail.com>
2021-05-01 13:20:59 +0600

    разделила urls

--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
+HOMEPAGE_URL = 'webapp/'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('webapp/', include('webapp.urls')),
+    # path('accounts/', include('accounts.urls')),
+    path('', RedirectView.as_view(url=HOMEPAGE_URL, permanent=False)),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
