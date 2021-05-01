5aa002f2549d474c3ef48ef1f6bf33554521de80
Aiganysh <aiganyshx@gmail.com>
2021-05-01 13:22:13 +0600

    Добавила Form

---
+++ hello/webapp/form.py
@@ -0,0 +1,14 @@
+from django import forms
+from webapp.models import Product, Review
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = ('name_product', 'category', 'description', 'picture')
+
+class ReviewForm(forms.ModelForm):
+
+    class Meta:
+        model = Review
+        fields = ('text_review', )
